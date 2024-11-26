1a7f428bdd9a66c8f0b9ffa510c3afa7f8975ddb
Nikolaus <nikolausrsaputra@gmail.com>
2024-11-26 23:03:38 +0700

    update register sql table file

--- register.sql
+++ register.sql
@@ -1,1 +1,7 @@
--- test
+CREATE TABLE  (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR (255),
+    email UNIQUE VARCHAR (255) UNIQUE,
+    nim UNIQUE VARCHAR (255) UNIQUE,
+    password VARCHAR (255)
+);
